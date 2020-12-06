57e6bf7f109bb1a667a40b29fddf38e641096a82
linddan <daniel.lindros@gig.com>
2020-12-06 10:04:39 +0100

    wip

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -11,18 +11,6 @@ export const isSpare = (roll1: Roll, roll2: Roll): boolean =>
 const createGetRollAtIndex = (rolls: Roll[]) => (index: number) =>
     index < 0 || index >= rolls.length ? UNROLLED : rolls[index];
 
-export const createNewFrame = (
-    roll1: Roll = UNROLLED,
-    roll2: Roll = UNROLLED,
-    roll3: Roll = UNROLLED,
-    total = 0
-): Frame => ({
-    roll1,
-    roll2,
-    roll3,
-    total,
-});
-
 // Create a new, empty, game
 export const createNewGame = (playerName: string): Game => ({
     id: uuid(),
@@ -33,9 +21,10 @@ export const createNewGame = (playerName: string): Game => ({
 });
 
 // Calculates the sum of rolls, up until stopIndex (if passed)
-export const calculateSum = (rolls: Roll[]): any => {
+export const calculateSum = (rolls: Roll[]): { sum: number; frames: Frame[] } => {
     const frames: Frame[] = [];
     const getRollAtIndex = createGetRollAtIndex(rolls);
+    const geAddValue = (roll: Roll) => (isRolled(roll) ? roll : 0);
 
     let gameSum = 0;
     let rollIndex = 0;
@@ -44,102 +33,44 @@ export const calculateSum = (rolls: Roll[]): any => {
         const isLastFrame = frame === 10;
         const currRoll = getRollAtIndex(rollIndex);
         const nextRoll = getRollAtIndex(rollIndex + 1);
-        const nextnextRoll = getRollAtIndex(rollIndex + 2);
-
+        const nextNextRoll = getRollAtIndex(rollIndex + 2);
+        const isStrikeFrame = isStrike(currRoll);
+        const isSpareFrame = isSpare(currRoll, nextRoll);
         let frameSum = currRoll;
 
         if (!isRolled(currRoll)) break;
 
-        if (isStrike(currRoll) || isSpare(currRoll, nextRoll)) {
-            // Strike
-            if (isRolled(nextRoll)) {
-                frameSum += nextRoll;
-            }
-            if (isRolled(nextnextRoll)) {
-                frameSum += nextnextRoll;
-            }
-            rollIndex++;
-            if (isSpare(currRoll, nextRoll)) {
-                rollIndex++;
-            }
+        if (isStrikeFrame || isSpareFrame) {
+            frameSum += geAddValue(nextRoll);
+            frameSum += geAddValue(nextNextRoll);
+            rollIndex = isSpareFrame ? rollIndex + 2 : rollIndex + 1;
         } else {
-            // Normal
-            if (isRolled(nextRoll)) {
-                frameSum += nextRoll;
-            }
+            frameSum += geAddValue(nextRoll);
             rollIndex += 2;
         }
+
+        // Update total sum
         gameSum += frameSum;
 
         // Add current frame to list
         frames.push({
             roll1: currRoll,
-            roll2: isStrike(currRoll) ? 0 : nextRoll,
-            roll3: isLastFrame ? nextnextRoll : UNROLLED,
+            roll2: isStrikeFrame ? 0 : nextRoll,
+            roll3: isLastFrame ? nextNextRoll : UNROLLED,
             total: gameSum,
         });
     }
     return { sum: gameSum, frames };
 };
 
-// export const createFrames = (rolls: Roll[]): Frame[] => {
-//     // [10, 5, 5, 4, 0]
-//     // [10]
-
-//     console.log('rolls', rolls);
-
-//     let rollIndex = 0;
-//     for (let i = 1; i <= 10; i++) {
-//         if (rollIndex >= rolls.length) break;
-
-//         // const isLastFrame = frames.length / 2 > 10;
-//         const isLastFrame = i === 10;
-//         const currRoll = getRollAtIndex(rollIndex);
-//         const nextRoll = getRollAtIndex(rollIndex + 1);
-//         const nextnextRoll = getRollAtIndex(rollIndex + 2);
-//         const total = calculateSum(rolls.slice(0, rollIndex + 1));
-
-//         if (isLastFrame) {
-//             frames.push({
-//                 roll1: currRoll,
-//                 roll2: nextRoll,
-//                 roll3: nextnextRoll,
-//                 total,
-//             });
-//             rollIndex++;
-//             continue;
-//         }
-//         if (isStrike(currRoll)) {
-//             frames.push({
-//                 roll1: currRoll,
-//                 roll2: 0,
-//                 roll3: UNROLLED,
-//                 total,
-//             });
-//             rollIndex++;
-//             continue;
-//         }
-//         frames.push({
-//             roll1: currRoll,
-//             roll2: nextRoll,
-//             roll3: UNROLLED,
-//             total,
-//         });
-//         rollIndex += 2;
-//     }
-
-//     console.log('frames', frames);
-//     return frames;
-// };
-
 // What symbol/number to show in the frame
 export const getRollResultSymbols = (
-    roll1: number,
-    roll2: number,
-    roll3: number
-): Array<string | number | undefined> => {
+    roll1: Roll,
+    roll2: Roll,
+    roll3: Roll
+): Array<string | number> => {
     // If user has 2 strikes in 10th frame
-    if (roll3 !== undefined) {
+    if (isRolled(roll3)) {
         return ['X', 'X', roll3];
     }
     if (isStrike(roll1)) {
